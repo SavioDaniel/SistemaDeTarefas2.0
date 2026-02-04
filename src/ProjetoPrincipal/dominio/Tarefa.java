270c50bc0470ca1364e561241147f06ae6b344bf
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-02-04 14:32:51 -0300

    Create Tarefa.java

---
+++ src/ProjetoPrincipal/dominio/Tarefa.java
@@ -0,0 +1,12 @@
+package ProjetoPrincipal.dominio;
+
+public class Tarefa {
+    public String nome;
+    public boolean concluido;
+
+    public  Tarefa(String nome){
+        this.nome = nome;
+        this.concluido;
+    }
+
+}
