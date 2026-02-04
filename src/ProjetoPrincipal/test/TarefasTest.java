31dd54286f8f9986fee2b4221e147a16c1b0ee58
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-02-04 14:07:15 -0300

    Create TarefasTest.java

---
+++ src/ProjetoPrincipal/test/TarefasTest.java
@@ -0,0 +1,51 @@
+package ProjetoPrincipal.test;
+import java.util.Scanner;
+
+
+public class TarefasTest {
+    static void main(String[] args) {
+     Scanner scanner = new Scanner(System.in);
+
+     while (true) {
+
+         System.out.println("--------------Bem-vindo ao MENU--------------");
+         System.out.println("1. Criar tarefa");
+         System.out.println("2. Concluir tarefa");
+         System.out.println("3. Excluir tarefa");
+         System.out.println("4. Listar tarefa");
+         System.out.println("5. Sair");
+         System.out.println("Escolha a opção desejada");
+         int opcao = scanner.nextInt();
+
+         switch (opcao) {
+
+             case 1: {
+                 System.out.println("Você escolheu a opção 1: Criar tarefa");
+                 break;
+             }
+             case 2: {
+                 System.out.println("Você escolheu a opção 2. Concluir tarefa");
+                 break;
+             }
+             case 3: {
+                 System.out.println("Você escolheu a opção 3. Excluir tarefa");
+                 break;
+             }
+             case 4: {
+                 System.out.println("Você escolheu a opção 4. Listar tarefa");
+                 break;
+             }
+             case 5: {
+                 System.out.println("Você escolheu a opção 5. Sair");
+                 scanner.close();
+                 return;
+             }
+             default: {
+                 System.out.println("Você não escolheu nenhum número do menu");
+             }
+
+         }
+
+     }
+    }
+}
