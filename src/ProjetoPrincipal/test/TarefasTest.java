e02dec48cf2bdbacf144d5057181d6b750cb52ed
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-02-05 14:39:32 -0300

    Update TarefasTest.java

--- src/ProjetoPrincipal/test/TarefasTest.java
+++ src/ProjetoPrincipal/test/TarefasTest.java
@@ -1,8 +1,6 @@
 package ProjetoPrincipal.test;
 
 import ProjetoPrincipal.dominio.GerenciadorTarefas;
-import ProjetoPrincipal.dominio.ExclusorTarefas;
-
 import java.util.Scanner;
 
 public class TarefasTest {
@@ -10,50 +8,51 @@ public class TarefasTest {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-
-        GerenciadorTarefas ger = new GerenciadorTarefas();
-        ExclusorTarefas exclusor = new ExclusorTarefas();
+        GerenciadorTarefas gerenciador = new GerenciadorTarefas();
 
         while (true) {
 
-            System.out.println("-------------- MENU --------------");
+            System.out.println("\n--------- MENU ---------");
             System.out.println("1. Criar tarefa");
             System.out.println("2. Concluir tarefa");
             System.out.println("3. Excluir tarefa");
             System.out.println("4. Listar tarefas");
             System.out.println("5. Sair");
-            System.out.print("Opção: ");
+            System.out.print("Escolha: ");
 
             int opcao = scanner.nextInt();
 
             switch (opcao) {
 
                 case 1:
-                    ger.criarTarefa();
+                    gerenciador.criarTarefa();
                     break;
 
                 case 2:
-                    ger.concluirTarefa();
+                    gerenciador.concluirTarefa();
                     break;
 
                 case 3:
-                    exclusor.excluir(ger);
+                    System.out.print("Digite o número da tarefa: ");
+                    int num = scanner.nextInt();
+
+                    // CORRIGIDO AQUI
+                    gerenciador.remover(num - 1);
+
                     break;
 
                 case 4:
-                    ger.listarTarefas();
+                    gerenciador.listarTarefas();
                     break;
 
                 case 5:
-                    System.out.println("Programa encerrado.");
+                    System.out.println("Saindo...");
                     scanner.close();
                     return;
 
                 default:
                     System.out.println("Opção inválida!");
             }
-
-            System.out.println();
         }
     }
 }
